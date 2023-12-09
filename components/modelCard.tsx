436e52630fc5fb62ed2e39fab34f6ad9da3047ef
Jack Tsai <jtsai0606@gmail.com>
2023-12-10 02:37:58 +0800

    fixed modelCard link

--- components/modelCard.tsx
+++ components/modelCard.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export interface ModelCardProps {
-  modelName : string;
+  modelName: string;
   modelAddress: string;
   totalSupply: number;
   nftMint: number;
@@ -9,9 +9,16 @@ export interface ModelCardProps {
   description: string;
 }
 
-const ModelCard: React.FC<ModelCardProps> = ({modelName, modelAddress, totalSupply, nftMint, title, description }) => {
+const ModelCard: React.FC<ModelCardProps> = ({
+  modelName,
+  modelAddress,
+  totalSupply,
+  nftMint,
+  title,
+  description,
+}) => {
   return (
-    <Link href={`/address`}>
+    <Link href={`/model/1/detail`}>
       <div className="card w-80 bg-black text-white rounded-lg shadow-lg overflow-hidden hover:scale-[1.02] hover:outline outline-cyan-500 outline-2	 transition">
         <div className="flex justify-between items-center">
           <h2 className="card-title p-4">{modelName}</h2>
@@ -23,15 +30,17 @@ const ModelCard: React.FC<ModelCardProps> = ({modelName, modelAddress, totalSupp
             alt="Shoes"
           />
         </figure>
-        
+
         {/* Content */}
         <div className="p-4 mt-7">
           <h2 className="text-2xl mb-4 font-bold">{title}</h2>
           <p className="mb-4 text-zinc-400">{description}</p>
           <div className="bg-black text-white  flex items-center justify-between ">
             <h2 className="">Token Supply</h2>
-            <span className="text-cyan-400 text-sm">{nftMint} / {totalSupply}</span>
-          </div>  
+            <span className="text-cyan-400 text-sm">
+              {nftMint} / {totalSupply}
+            </span>
+          </div>
         </div>
       </div>
     </Link>
