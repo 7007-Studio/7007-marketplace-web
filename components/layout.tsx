2af6c78f68e41f179ae60bb934b7c96426066691
Jack Tsai <jtsai0606@gmail.com>
2024-02-22 17:39:15 -0800

    adjust launchpad page

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,13 +11,13 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div data-theme="light-7007" className={`${barlow.className} bg-base-100`}>
       <Navbar />
-      <div className="flex md:hidden absolute top-0 -z-10 w-full h-full bg-base-100 justify-center items-center">
+      <div className="flex lg:hidden absolute top-0 -z-10 w-full h-full bg-base-100 justify-center items-center">
         <div className="p-10 text-center">
           For now, 7007 studio is on desktop only. Mobile version will launch
           soon.
         </div>
       </div>
-      <main className="max-md:hidden flex min-h-screen flex-col lg:py-10 md:px-10 lg:px-24 mx-auto">
+      <main className="max-lg:hidden flex min-h-screen flex-col lg:py-10 md:px-10 lg:px-24 mx-auto">
         {children}
       </main>
     </div>
--- components/model/launched.tsx
+++ components/model/launched.tsx
@@ -10,9 +10,9 @@ const Launched = ({ modelIndex }: { modelIndex: number | string }) => {
     "$eth Accumulated Reward",
   ];
   return (
-    <div className="w-full min-w-[533px] py-12 px-12 rounded-md bg-white">
+    <div className="w-full min-w-[360px] 2xl:min-w-[533px] py-12 px-12 rounded-md bg-white">
       <h3 className="heading-md">Launched</h3>
-      <div className="grid grid-cols-2 py-12 gap-y-10">
+      <div className="grid grid-cols-2 py-12 gap-x-8 gap-y-10">
         {items.map((i) => (
           <div key={i} className="flex flex-col">
             <h2 className="text-lg mb4-4">-</h2>
