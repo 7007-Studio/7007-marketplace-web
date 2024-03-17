5ff410a8137bb9e0a7dd23f5cd5bcd9748429326
veraliu <veraaaaaliuuuuu@gmail.com>
2024-03-17 16:12:50 +0800

    add infinite scroll

--- app/(home)/erc-7007/collection.tsx
+++ app/(home)/erc-7007/collection.tsx
@@ -1,11 +1,17 @@
 "use client";
+import { useState, useEffect } from "react";
 import { useAccount } from "wagmi";
-
 import { ModelIndex } from "@/constants";
 import useNftContract from "@/hooks/useNftContract";
 import useNftCollection from "@/hooks/useNftCollection";
 import NFTCard from "@/components/nftCard";
 import EmptyCard from "@/components/emptyCard";
+
+// variables
+const ITEMS_PER_PAGE = 8;
+const SCROLL_THRESHOLD = 800;
+const INITIAL_EMPTY_CARDS_COUNT = 1;
+let PREV_SCROLL_Y= 0;
 
 const Collection = () => {
   const { chain } = useAccount();
@@ -13,30 +19,52 @@ const Collection = () => {
     modelIndex: ModelIndex,
     chainId: chain?.id,
   });
-  const emptyCardList = [...Array(1).keys()];
+  const [tokenIds, setTokenIds] = useState<any[]>([]);
+  const [startIndex, setStartIndex] = useState(1);
+  const { tokenIds: initialTokenIds } = useNftCollection({ nftContract });
+  const emptyCardList = Array.from({ length: INITIAL_EMPTY_CARDS_COUNT });
 
-  const { tokenIds } = useNftCollection({ nftContract });
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY + 500;
+    if (currentScrollY - PREV_SCROLL_Y > SCROLL_THRESHOLD) {
+      PREV_SCROLL_Y = currentScrollY;
+      setStartIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (nftContract) {
+      setTokenIds(initialTokenIds.slice(0, ITEMS_PER_PAGE * startIndex));
+    }
+  }, [nftContract, startIndex, initialTokenIds]);
+
+  useEffect(() => {
+    if (nftContract) {
+      setTokenIds(initialTokenIds.slice(0, ITEMS_PER_PAGE));
+    }
+  }, [initialTokenIds, nftContract]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
-      {(nftContract && (
-       <div className="columns-3 max-w-[828px] xl:columns-4 xl:max-w-[1106px]">
-          {tokenIds.map((id) => (
-            <NFTCard
-              key={`${nftContract}-${id}`}
-              nftContract={nftContract}
-              tokenId={BigInt(id)}
-            />
-          )) || emptyCardList.map((l) => <EmptyCard key={l} />)}
-        </div>
-      )) || (
-        <div className="columns-3 max-w-[828px] xl:columns-4 xl:max-w-[1106px]">
-          {emptyCardList.map((l) => (
-            <EmptyCard key={l} />
-          ))}
-        </div>
-      )}
-    </> 
+      <div className="columns-3 max-w-[828px] xl:columns-4 xl:max-w-[1106px]">
+        {tokenIds.length > 0
+          ? tokenIds.map((id) => (
+              <NFTCard
+                key={`${nftContract}-${id}`}
+                nftContract={nftContract}
+                tokenId={BigInt(id)}
+              />
+            ))
+          : emptyCardList.map((_, index) => <EmptyCard key={index} />)}
+      </div>
+    </>
   );
 };
 
