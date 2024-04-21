66bb649244e6b6d0f263674accca978eb76c3424
whereissam <sam135642@gmail.com>
2024-04-21 21:07:06 +0800

    fix: zustand typescript

--- app/create/collection/store.tsx
+++ app/create/collection/store.tsx
@@ -1,32 +1,14 @@
 import create from 'zustand';
 
-const useGetImageStore = create((set) => ({
+interface ImageStoreState {
+  uploadImages: string[]; // Assuming you store image URLs or paths
+  setUploadImages: (newImages: string[]) => void;
+}
+
+const useGetImageStore = create<ImageStoreState>((set) => ({
     uploadImages: [],
     setUploadImages: (newImages) => { set({ uploadImages: newImages }) },
 }));
 
-// Define a store for the collection description
-const useCollectionDescriptionStore = create((set) => ({
-    collectionDescription: '',
-    setCollectionDescription: (description) => set({ collectionDescription: description }),
-  }));
-  
-  // Define a store for the total supply
-  const useTotalSupplyStore = create((set) => ({
-    totalSupply: '',
-    setTotalSupply: (supply) => set({ totalSupply: supply }),
-  }));
-  
-  // Define a store for the creator earnings percentage
-  const useCreatorEarningsStore = create((set) => ({
-    creatorEarnings: '',
-    setCreatorEarnings: (earnings) => set({ creatorEarnings: earnings }),
-  }));
 
-  // Define a store for the creator earnings percentage
-  const useCreatorModelNameStore = create((set) => ({
-    creatorModelName: '',
-    setcreatorModelName: (name) => set({ creatorModelName: name }),
-  }));
-  
-  export { useCollectionDescriptionStore, useTotalSupplyStore, useCreatorEarningsStore, useGetImageStore, useCreatorModelNameStore };
+  export { useGetImageStore };
